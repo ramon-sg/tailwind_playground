9205c81ac2cdeb4c8382208d1f33be61cb1bbcf4
Ramón Soto <ddsstr@gmail.com>
2023-01-15 19:41:24 -0300

    Add hot reload

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-console.log('Hi :)')
+/**
+ * add hot reload
+ * @see {@link https://github.com/parcel-bundler/parcel/issues/7241#issuecomment-986410024}
+ */
+if (module.hot) {
+  module.hot.accept(() => {
+    location.reload();
+  });
+}
+
+console.log("Hi :)");
